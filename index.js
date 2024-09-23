0839b7e52ed2f1f8fae426a80fc31aaf3f5a1a93
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-09-23 13:04:22 +0600

    get : single job by id

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nbrjeuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxgrzuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,10 +31,10 @@ async function run() {
     // await client.connect();
     const database = client.db("jobifyDB");
 
-    const companiesCollection =database.collection("companies");
+    const companiesCollection = database.collection("companies");
+    const jobsCollection = database.collection("jobs");
 
-    
-    app.get('/companies/top', async (req, res) => {
+    app.get("/companies/top", async (req, res) => {
       try {
         const companies = await companiesCollection
           .find()
@@ -47,15 +47,7 @@ async function run() {
       }
     });
 
-    
-    
-    
-    
-    
     console.log("You successfully connected to MongoDB!");
-
-    const companiesCollection = database.collection("companies");
-    const jobsCollection = database.collection("jobs");
 
     // Get Company by ID
     app.get("/companies/:id", async (req, res) => {
@@ -80,20 +72,34 @@ async function run() {
       res.send(result);
     });
 
+    // get single job
+    app.get("/jobs/:id", async (req, res) => {
+      const { id } = req.params; // Get ID from request parameters
+      try {
+        const result = await jobsCollection.findOne({ _id: id }); // Treat _id as a string
+    
+        if (!result) {
+          return res.status(404).send("Job not found");
+        }
+    
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+      }
+    });
+    
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-
   } finally {
   }
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
   res.send("Jobify server");
-
 });
 
 app.listen(port, () => {
